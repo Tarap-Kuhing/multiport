7debdc14a6782020f6c74ef78d13352466b65e4a
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-15 17:39:54 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -79,19 +79,17 @@ source /var/lib/ipvps.conf
 domain=$(cat /usr/local/etc/xray/domain)
 emailcf=$(cat /usr/local/etc/xray/email)
 clear
-systemctl stop xray
+systemctl stop nginx
 echo -e "\e[0;32mStart renew your Certificate SSL\e[0m"
 sleep 1
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
-systemctl start xray
+systemctl start nginx
 echo Done
 sleep 0.5
 echo -e "[${GREEN}Done${NC}]"
-else
 echo -e "\e[1;32mPort 80 is used\e[0m"
 echo -e "\e[1;31mBefore renew domains, make sure port 80 is not used, if you are not sure whether port 80 is in use, please type info to see the active port.\e[0m"
 sleep 1
-fi
