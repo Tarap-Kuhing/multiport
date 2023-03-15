b3e71e1793820a279a7e3b99a534d4f8cf427b64
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-15 23:59:34 +0800

    Update backup.sh

--- system/backup.sh
+++ system/backup.sh
@@ -1,44 +1,67 @@
 #!/bin/bash
-echo -e "\e[32mloading...\e[0m"
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
 clear
-IP=$(wget -qO- icanhazip.com);
+IP=$(wget -qO- ipinfo.io/ip);
 date=$(date +"%Y-%m-%d")
 clear
-echo " Enter Your Email To Receive Message"
-read -rp " Email: " -e email
-sleep 1
-echo Directory Created
-mkdir /root/backup
-sleep 1
-echo Start Backup
+email=$(cat /home/email)
+if [[ "$email" = "" ]]; then
+echo "Masukkan Email Untuk Menerima Backup"
+read -rp "Email : " -e email
+cat <<EOF>>/home/email
+$email
+EOF
+fi
 clear
+echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
+rm -rf /root/backup
+mkdir /root/backup
 cp /etc/passwd backup/
 cp /etc/group backup/
 cp /etc/shadow backup/
 cp /etc/gshadow backup/
-cp -r /etc/wireguard backup/wireguard
-cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
-cp -r /var/lib/ backup
-cp -r /usr/local/etc/xray backup/xray
-cp -r /etc/trojan-go backup/trojan-go
-cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
-cp -r /home/vps/public_html backup/public_html
+cp /etc/msmtprc backup/
+cp /home/email backup/
+cp /etc/ppp/chap-secrets backup/chap-secrets
+cp -r /var/lib/crot/ backup/crot
+cp -r /etc/xray backup/xray
+cp -r /home/vps backup/vps
+cp -r /root/domain backup/domain
+cd /root
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
 url=$(rclone link dr:backup/$IP-$date.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "The following is a link to your vps data backup file.
-
-Your VPS IP $IP
-
-$link
-
-If you want to restore data, please enter the link above.
-
-Thank You For Using Our Services" | mail -s "Backup Data" $email
+echo -e "
+Detail Backup 
+==================================
+IP VPS        : $IP
+Link Backup   : $link
+Tanggal       : $date
+==================================
+" | mail -s "Backup Data" $email
 rm -rf /root/backup
 rm -r /root/$IP-$date.zip
-echo "Done"
-echo "Please Check Your Email"
+clear
+echo -e "
+Detail Backup 
+==================================
+IP VPS        : $IP
+Link Backup   : $link
+Tanggal       : $date
+==================================
+"
+echo "Silahkan cek Kotak Masuk $email"
