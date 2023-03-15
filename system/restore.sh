f8e3cb29221565814867e232d7bd97c2e248b260
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-15 10:57:57 +0800

    Update restore.sh

--- system/restore.sh
+++ system/restore.sh
@@ -1,5 +1,4 @@
-#wget https://github.com/${GitUser}/
-GitUs#!/bin/bash
+#!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
