7a4baed003ff0ed6831641c509ec91e777278df9
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-16 15:08:56 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -79,17 +79,13 @@ source /var/lib/ipvps.conf
 domain=$(cat /usr/local/etc/xray/domain)
 emailcf=$(cat /usr/local/etc/xray/email)
 clear
-systemctl stop nginx
-echo -e "\e[0;32mStart renew your Certificate SSL\e[0m"
-sleep 1
-/root/.acme.sh/acme.sh --upgrade --auto-upgrade
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
-systemctl start nginx
-echo Done
-sleep 0.5
-echo -e "[${GREEN}Done${NC}]"
-echo -e "\e[1;32mPort 80 is used\e[0m"
-echo -e "\e[1;31mBefore renew domains, make sure port 80 is not used, if you are not sure whether port 80 is in use, please type info to see the active port.\e[0m"
-sleep 1
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
+bash acme.sh --register-account -m merahjambo@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+restart
