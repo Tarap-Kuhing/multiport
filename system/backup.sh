8b53e3c2331e2ca2e8337016700513884f2e004b
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-16 13:09:12 +0800

    Update backup.sh

--- system/backup.sh
+++ system/backup.sh
@@ -35,10 +35,9 @@ cp /etc/msmtprc backup/
 cp /home/email backup/
 #cp /etc/ppp/chap-secrets backup/chap-secrets
 cp -r /var/lib/ backup
-cp -r /usr/local/etc/xray backup/xray
+#cp -r /usr/local/etc/xray backup/xray
 cp -r /etc/xray backup/xray
 cp -r /home/vps backup/vps
-cp -r /root/xray/domain backup
 #cp -r /home/vps/public_html backup/public_html
 cd /root
 cd /root
