420ae1a86feac35ce9b0395eae3cc8bf4c1025a8
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-16 11:16:01 +0800

    Update backup.sh

--- system/backup.sh
+++ system/backup.sh
@@ -36,9 +36,12 @@ cp /home/email backup/
 cp /etc/ppp/chap-secrets backup/chap-secrets
 cp -r /var/lib/ backup
 cp -r /etc/xray backup/xray
-cp -r /etc/xray/vmess backup/xray/vmess
+cp -r /etc/trojan backup/trojan
+cp -r /etc/vmess backup/vmess
+cp -r /etc/vless backup/vless
 cp -r /home/vps backup/vps
 cp -r /root/domain backup/domain
+cp -r /home/vps/public_html backup/public_html
 cd /root
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
