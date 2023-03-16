37d7b291ad861406d8a55c1e76a57db4eeaeff36
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-16 20:16:03 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -103,17 +103,18 @@ echo start
 sleep 0.5
 source /var/lib/ipvps.conf
 domain=$(cat /usr/local/etc/xray/domain)
-clear
-systemctl stop xray
-systemctl stop nginx
+systemctl enable xray
+systemctl enable nginx
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd
 echo -e "\e[0;32mStart renew your Certificate SSL\e[0m"
 sleep 1
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server $acmeh
 #/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --listen-v6
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
-systemctl start xray
-systemctl start nginx
+systemctl restart xray
+systemctl restart nginx
 echo Done
 sleep 0.5
 echo -e "[${GREEN}Done${NC}]"
