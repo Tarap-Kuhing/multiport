a20ec916ed70744eadb14579afca179defd9624f
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-16 17:24:09 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -78,15 +78,24 @@ sleep 0.5
 source /var/lib/ipvps.conf
 domain=$(cat /usr/local/etc/xray/domain)
 emailcf=$(cat /usr/local/etc/xray/email)
-systemctl stop xray
+# // UPDATE CERT SSL
 clear
-cd /root/
-wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
-bash acme.sh --install
-rm acme.sh
-cd .acme.sh
-echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
-bash acme.sh --register-account -m merahjambo@gmail.com
-bash acme.sh --issue --standalone -d $domain --force
-bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
-systemctl restart xray
+echo
+echo "Automatical Update Your Certificate SSL"
+sleep 0.3
+echo Starting Update SSL Certificate
+sleep 0.3
+
+# // STOP XRAY
+systemctl stop xray.service
+systemctl stop xray@none
+
+# // GENERATE CERT
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --listen-v6
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
+
+# // RESTART XRAY
+systemctl restart xray.service
+systemctl restart xray@none
