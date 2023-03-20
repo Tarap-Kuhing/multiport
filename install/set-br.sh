f2abeb4f8dc436d7b5585eb5aa1deeafbf99043b
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-03-20 18:21:37 +0800

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -8,6 +8,23 @@ cd wondershaper
 make install
 cd
 rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+account default
+host smtp.gmail.com
+port 587
+auth on
+user bckupvpns@gmail.com
+from bckupvpns@gmail.com
+password Yangbaru1
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
 wget -O autobackup "https://raw.githubusercontent.com/Tarap-Kuhing/multiport/main/system/autobackup.sh"
 wget -O backup "https://raw.githubusercontent.com/Tarap-Kuhing/multiport/main/system/backup.sh"
