db82cc1531f3cf321d44d0bf758f4f727f0f43bf
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-04 20:21:42 +0700

    Add files via upload

--- websocket-python/websocket.sh
+++ websocket-python/websocket.sh
@@ -1,54 +1,57 @@
 #!/bin/bash
-# Proxy For Edukasi & Imclass
-# Mod By TARAP KUHING
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
+clear
+echo Installing Websocket-SSH Python
+sleep 1
+echo Sila Tunggu Sebentar...
+sleep 0.5
+cd
 
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/WEBSOCKET/ws-dropbear
-chmod +x /usr/local/bin/ws-dropbear
-
-# Installing Service
-cat > /etc/systemd/system/ws-dropbear.service << END
+# // SYSTEM WEBSOCKET HTTPS
+cat <<EOF> /etc/systemd/system/ws-https.service
 [Unit]
-Description=Python Proxy Mod By TARAP KUHING
+Description=Python Proxy
 Documentation=https://t.me/Hendra2012
 After=network.target nss-lookup.target
+
 [Service]
 Type=simple
 User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear
+Restart=on-failure
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-https
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# // SYSTEM WEBSOCKET HTTP
+cat <<EOF> /etc/systemd/system/ws-http.service
+[Unit]
+Description=Python Proxy
+Documentation=https://t.me/Hendra2012
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-http
 Restart=on-failure
 [Install]
 WantedBy=multi-user.target
-END
+EOF
 
-systemctl daemon-reload
-systemctl enable ws-dropbear.service
-systemctl start ws-dropbear.service
-systemctl restart ws-dropbear.service
-
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/WEBSOCKET/ws-ovpn.py
-chmod +x /usr/local/bin/ws-ovpn
-
-# Installing Service
-cat > /etc/systemd/system/ws-ovpn.service << END
+# // SYSTEM WEBSOCKET OVPN
+cat <<EOF> /etc/systemd/system/ws-ovpn.service
 [Unit]
-Description=Python Proxy Mod By TARAP KUHING
+Description=Python Proxy
 Documentation=https://t.me/Hendra2012
 After=network.target nss-lookup.target
+
 [Service]
 Type=simple
 User=root
@@ -57,35 +60,25 @@ AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
 Restart=on-failure
+
 [Install]
 WantedBy=multi-user.target
-END
+EOF
 
+# // PYTHON WEBSOCKET TLS && NONE
+wget -q -O /usr/local/bin/ws-https https://raw.githubusercontent.com/Tarap-Kuhing/multiport/main/websocket-python/ws-https; chmod +x /usr/local/bin/ws-https
+
+# // PYTHON WEBSOCKET DROPBEAR
+wget -q -O /usr/local/bin/ws-http https://raw.githubusercontent.com/Tarap-Kuhing/multiport/main/websocket-python/ws-http; chmod +x /usr/local/bin/ws-http
+
+# // PYTHON WEBSOCKET OVPN
+wget -q -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/multiport/main/websocket-python/ws-ovpn; chmod +x /usr/local/bin/ws-ovpn
+
+# // RESTART && ENABLE SSHVPN WEBSOCKET TLS
 systemctl daemon-reload
+systemctl enable ws-https
+systemctl restart ws-https
+systemctl enable ws-http
+systemctl restart ws-http
 systemctl enable ws-ovpn
 systemctl restart ws-ovpn
-
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/WEBSOCKET/ws-stunnel
-chmod +x /usr/local/bin/ws-stunnel
-
-# Installing Service
-cat > /etc/systemd/system/ws-stunnel.service << END
-[Unit]
-Description=Python Proxy Mod By TARAP KUHING
-Documentation=https://t.me/Hendra2012
-After=network.target nss-lookup.target
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel
-Restart=on-failure
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl enable ws-stunnel.service
-systemctl start ws-stunnel.service
-systemctl restart ws-stunnel.service
